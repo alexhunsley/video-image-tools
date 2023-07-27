a2a58df96d59b619431c4c64f52833e731a08c44
Alex Hunsley <alex.hunsley@gmail.com>
2023-07-27 16:18:58 +0100

    Update ffmpeg command from gpt

--- video_guides/overlay_guide.sh
+++ video_guides/overlay_guide.sh
@@ -20,17 +20,32 @@ fi
 
 for file in "${files[@]}"; do
 
-    base_name=$(basename "$file" .mp4)
+	if [[ "$file" == *"_i."* ]]; then
+		continue
+	fi
 
-	ffmpeg -y -i "$file" -loop 1 -i "$image_path" \
-	   -filter_complex "\
-	    [0:v]scale='min(768,iw)':-2,hue=s=0,negate,setsar=1[video]; \
-	    [1:v][video]scale2ref[img][mv]; \
-	    [mv][img]overlay=eof_action=pass[ovr]; \
-	    [ovr]drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=48:text='%{eif\:mod(t,3600)/60\:d\:2}\:%{eif\:mod(t,60)\:d\:2}\:%{eif\:mod(t*100,100)\:d\:2}':fontcolor=red@0.8:x=10:y=10[txt]; \
-	    [txt]drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=48:text='$file':fontcolor=red@0.8:x=10:y=70:enable='between(t,0,1)'[out]; \
-	    [0:v]scale=-1:40,fps=5[small]; \
-	    [out][small]overlay=W-w-10:10[vout]" \
-	   -map "[vout]" -r 5 "${base_name}_i.mp4"
+    base_name=$(basename "$file" .mp4)
+    output_filename="${base_name}_i.mp4"
+
+	if [ -f "$output_filename" ]; then
+		echo "The file '${output_filename}' already exists, skipping."
+	else
+		# echo "PROCESSING (dry run XXXX) ${file}"
+
+		# full invert/bw/rescale/time+title overlay/colour thumb TR
+		ffmpeg -y -i "$file" -loop 1 -i "$image_path" \
+		   -filter_complex "\
+		    [0:v]scale='min(768,iw)':-2,hue=s=0,negate,setsar=1[video]; \
+		    [1:v][video]scale2ref[img][mv]; \
+		    [mv][img]overlay=eof_action=pass[ovr]; \
+		    [ovr]drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=48:text='%{eif\:mod(t,3600)/60\:d\:2}\:%{eif\:mod(t,60)\:d\:2}\:%{eif\:mod(t*100,100)\:d\:2}':fontcolor=red@0.8:x=10:y=70[txt]; \
+		    [txt]drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:fontsize=48:text='$file':fontcolor=red@0.8:x=10:y=130:enable='between(t,0,3)'[out]; \
+		    [0:v]scale=-1:40,fps=5[small]; \
+		    [out][small]overlay=W-w-10:10[vout]" \
+		   -map "[vout]" -map 0:a? -r 5 "${output_filename}"
+
+
+		# no invert/bw
+	fi
 
 done
