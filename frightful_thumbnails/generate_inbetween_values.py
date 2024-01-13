9d53e36a69f8a4b185dcc2c12d08a0bc320cb73f
Alex Hunsley <alex.hunsley@gmail.com>
2024-01-13 12:50:26 +0000

    M in N progress.

--- frightful_thumbnails/generate_inbetween_values.py
+++ frightful_thumbnails/generate_inbetween_values.py
@@ -6,7 +6,7 @@ def debug(msg, end='\n'):
         print(msg, end=end)
 
 # N is number of value total, M is number we want to pick (evenly spaced in N)
-def select_values(N, M, closed = True, allow_optimisation = True, is_inner = False):
+def select_values(result_array, N, M, start_index = 0, closed_start = True, closed_end = True, allow_optimisation = True, is_inner = False):
     """
     Select M values from N.
 
@@ -41,28 +41,72 @@ def select_values(N, M, closed = True, allow_optimisation = True, is_inner = Fal
     # >>> select_values(11, 9)
     # >>> select_values(11, 10)
 
+    if N ==0 or M == 0:
+        return
+
+    original_n = N
+
+    if closed_start:
+        # if closed_start is true, we're not in a inner call, so we know the start index is 0
+        result_array.append(0) 
+    # values = [0] if closed_start else []
+
+    # the inner call to this func has false for both these vars so we won't end up in here on inner call
+    if closed_start or closed_end:
+        use_start_index = 1 if closed_start else 0
+
+        if closed_start:
+            N -= 1
+            M -= 1
+
+        if closed_end:
+            N -= 1
+            M -= 1
+
+        print(f" BEFORE Inner call, result_array = {result_array}, and calling with N, M = {N} {M}")
+        select_values(result_array, N, M, start_index = use_start_index, closed_start = False, closed_end = False)
+        print(f" AFTER Inner call, result_array = {result_array}")
+
+        if closed_end:
+            result_array.append(original_n - 1)
+
+        return
+
     debug(f"====================== is_inner = {is_inner}")
     debug(f"N: {N} M: {M}")
 
     # avoid division by zero in degenerate case M = 1
     if M == 1:
-        debug(f"Got M = 1, so returning N // 2 = [{N // 2}]")
-        return [N // 2]
+        debug(f"Got M = 1, so appending N // 2 = [{N // 2}]")
+        result_array.append(N // 2)
+        return
 
-    if not closed and not is_inner:
-    # if not closed:
-        # we're not having items at first, last index, so add 2 to M
-        print(f" ()() adding 2 to M, now got M = {M}")
-        M += 2
+    # if not closed and not is_inner:
+    # # if not closed:
+    #     # we're not having items at first, last index, so add 2 to M
+    #     print(f" ()() adding 2 to M, now got M = {M}")
+    #     M += 2
 
-    D = (N - 1) // (M - 1)
+    # use_m = M - 1
+    use_m = M + 1
+
+    use_n = N + 1
+
+    # if not closed_start: 
+    #     use_m += 1
+    # if not closed_end: 
+    #     use_m += 1
+
+    # with open start and end, we want to use M + 1?!
+    # so add one to M - 1 for each of start, end that is open.
+    D = use_n // use_m
     debug(f"D: {D}")
     
     # Calculate the remainder to adjust spacing
-    R = (N - 1) % (M - 1)
+    R = use_n % use_m
     debug(f"R: {R}")
 
-    S = (M - 1) // R if R > 0 else None
+    S = use_m // R if R > 0 else None
     debug(f"S: {S}")
 
     # optimisation: start with entire range if there are more values than holes
@@ -73,8 +117,10 @@ def select_values(N, M, closed = True, allow_optimisation = True, is_inner = Fal
         # new_n = N if closed else N - 2
         # gap_indexes = select_values(new_n, new_n - M, closed = False, is_inner = True)
         # for 4, 3 this is called with 2, 1
-        gap_indexes = select_values(N - 2, N - M, closed = False, is_inner = True)
 
+        # gap_indexes = select_values(N - 2, N - M, closed = False, is_inner = True)
+        gap_indexes = select_values(N - 2, N - M, is_inner = True)
+        
         debug(f">>>>>>>>> gap_indexes: {gap_indexes} count = {len(gap_indexes)}")
 
         complete_range = range(N)
@@ -92,20 +138,22 @@ def select_values(N, M, closed = True, allow_optimisation = True, is_inner = Fal
 
     # Initialize the list of values, starting with 0
     
-    values = [0] if closed else []
-    
+    # values = [0] if closed else []
+    debug(f">>>>>>>>> NO optimising")
+
     
 
     spaces = N - M
     debug(f"spaces: {spaces}")
 
     # Current value
-    current_value = 0
+    current_value = 0 if closed_start else -1
 
     # every time bump % S == 0 we increment current_value by 1
     bump_index = 0
     
-    for idx in range(0, M - 2):
+    # for idx in range(0, M - 2):
+    for idx in range(0, M):
         # Add the interval D to the current value
         current_value += D
         
@@ -120,34 +168,49 @@ def select_values(N, M, closed = True, allow_optimisation = True, is_inner = Fal
     #         remainder -= 1
         
     #     # Add the current value to the list
-        values.append(current_value)
+        result_array.append(current_value + start_index)
+        # result_array.append(current_value)
     
     # # Always include N as the last value
     # values.append(N)
 
-    if closed:  
-        values.append(N - 1)
+    # if closed:  
+    #     values.append(N - 1)
 
-    return values
+    # return values
 
 
-def run(N, M, closed = True, allow_optimisation = True):
+def run(N, M, closed_start = True, closed_end = True, allow_optimisation = True):
     debug("------------------------------------")
     debug("------------------------------------")
     debug("\n")
 
-    result = select_values(N, M, closed, allow_optimisation)
+    result = []
+# result_array, N, M, start_index = 0, closed_start = True, closed_end = True, allow_optimisation = True, is_inner = False)
+    select_values(result, N, M, closed_start = closed_start, closed_end = closed_end, allow_optimisation = False)
 
-    debug(f"-->> for N: {N}, M: {M}, closed: {closed}, allow_optimisation = {allow_optimisation}: {result}  ", end='')
+    debug(f"-->> for N: {N}, M: {M}, closed_st: {closed_start}, closed_end = {closed_end}, allow_optimisation = {allow_optimisation}: {result}  ", end='')
 
     # asci repr
-    for i in range(N):
-        if i in result:
-            print("*", end='')
-        else:
-            print(".", end='')
 
     print()
+
+    result_str = ""
+    for i in range(N):
+        print(f"Checking i = {i}  ... ", end='')
+        if i in result:
+            print("Found i in results, adding *")
+            result_str += "*"
+            # print("*", end='')
+        else:
+            print("Found NO i in results, adding .")
+            result_str += "."
+            # print(".", end='')
+    print()
+
+    print(f"__x {result_str} x__")
+
+    return result_str
 
 # Example Usage
 # run(3, 3)
@@ -173,9 +236,46 @@ def run(N, M, closed = True, allow_optimisation = True):
 # run(4, 3, allow_optimisation = False)
 
 
-run(4, 3, closed = False)
-run(4, 3, closed = False, allow_optimisation = False)
+# run(4, 3, closed_start = False, closed_end = False)
 
+# run(4, 2, closed_start = False, closed_end = False)
+
+# run(4, 1, closed_start = False, closed_end = False)
+
+
+# run(6, 3, closed_start = False, closed_end = False)
+
+# run(6, 3, closed_start = True, closed_end = True)
+
+
+# # these seem to work ok now
+# run(11, 4, closed_start = False, closed_end = False)
+# run(11, 4, closed_start = True, closed_end = True)
+# run(11, 4, closed_start = True, closed_end = False)
+# run(11, 4, closed_start = False, closed_end = True)
+
+# run(33, 6, closed_start = False, closed_end = False)
+# run(33, 6, closed_start = True, closed_end = True)
+# run(33, 6, closed_start = True, closed_end = False)
+# run(33, 6, closed_start = False, closed_end = True)
+
+# run(3, 0)
+# run(2, 0)
+# run(1, 0)
+# run(0, 0)
+
+def exhastive_test():
+    for n in range(2, 4):
+        # choosing 2 and up, since we've got closed at both ends currently
+        for m in range(2, n + 1):
+            # print(f"N: {n} M: {m}")
+            result_str = run(n, m)
+            print(result_str.count("*"))
+
+
+# exhastive_test()
+
+run(3, 2)
 
 # # run(11, 10)
 # run(4, 3)
